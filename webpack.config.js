e71a47c01caa94f641670812f0288b0c6ccbcea9
finn <36335479+Feenerys@users.noreply.github.com>
2025-07-19 16:41:37 +1000

    Update template (#1)
    * feat: add jest and babel configuration with a test file
    
    * feat: add scripts to package.json for easy build, serve, and deploy to github pages
    
    * feat: add eslint and its global variables
    
    * feat: add prettier and eslint-config-prettier so that no rules conflict
    
    * feat: add script for easy formatting of all files using prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import { resolve } from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
+export const mode = "development";
+export const entry = "./src/index.js";
+export const output = {
+  filename: "main.js",
+  // eslint-disable-next-line no-undef
+  path: resolve(__dirname, "dist"),
+  clean: true,
+};
+export const devtool = "eval-source-map";
+export const devServer = {
+  watchFiles: ["./src/template.html"],
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/template.html",
+  }),
+];
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    },
+    {
+      test: /\.html$/i,
+      loader: "html-loader",
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: "asset/resource",
+    },
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
 };
